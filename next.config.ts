f1164c4de2b409dd0db41d708c12881a66cd9847
unk-pn <zver2006.11@gmail.com>
2026-02-08 21:21:27 +0300

    hotfix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  i18n: {
-    locales: ["en", "ru"],
-    defaultLocale: "en",
-    localeDetection: false,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
